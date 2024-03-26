9fb80b3488acbf38be457aee839abcdf72793eb2
Xavier MANTELLATO <xavier.mantellato@arkance-systems.com>
2024-03-26 10:59:41 +0100

    update to test

--- __init__.py
+++ __init__.py
@@ -32,12 +32,12 @@ def extract_commits():
     response = urlopen('https://api.github.com/repos/Vexco/5MCSI_Metriques/commits')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
-    results = []
-    for list_element in json_content.get('list', []):
-        commit_value = list_element.get('commit')
-        date_value = list_element.get('author', {}).get('date') # Conversion de Kelvin en °c 
-        results.append({'Commit': commit_value, 'Date': date_value})
-    return jsonify(results=results)
+    # results = []
+    # for list_element in json_content.get('list', []):
+    #     commit_value = list_element.get('commit')
+    #     date_value = list_element.get('author', {}).get('date') # Conversion de Kelvin en °c 
+    #     results.append({'Commit': commit_value, 'Date': date_value})
+    return jsonify(json_content)
   
 @app.route("/rapport/")
 def mongraphique():
